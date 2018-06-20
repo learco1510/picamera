887c847f8f80f99fbbb661f1b1d22fe006333b4f
learco1510 <learco@post.bgu.ac.il>
2018-06-20 13:56:01 +0300

    first upload

---
+++ sketch_may28_copy_arduiono_code/sketch_may28_copy_arduiono_code.ino
@@ -0,0 +1,19 @@
+void setup() {
+  pinMode(12, OUTPUT); 
+  digitalWrite(12, HIGH); // turn the LED on (HIGH is the voltage level) 
+  delay(3000); // wait for a second
+  digitalWrite(12, LOW); // turn the LED off by making the voltage LOW 
+  delay(2000); // wait for a second 
+  digitalWrite(12, HIGH); // turn the LED on (HIGH is the voltage level) 
+  delay(3000); // wait for a second 
+  digitalWrite(12, LOW); // turn the LED off by making the voltage LOW 
+  delay(2000); // wait for a second
+}
+
+void loop() {
+  digitalWrite(12, HIGH); // turn the LED on (HIGH is the voltage level) 
+  delay(1000); // wait for a second 
+  digitalWrite(12, LOW); // turn the LED off by making the voltage LOW 
+  delay(1000); // wait for a second
+
+}
